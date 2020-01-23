b1471e55a7d03e7ad21c1ccc25745d6008d45568
Joshua Ronaldo <joshuaronaldo088@gmail.com>
2020-01-23 13:21:00 +0700

    configuring the first page

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+//        print(String(format: "%.2f m", sender.value))
+        heightLabel.text = String(format: "%.2f m", sender.value)
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+//        print(String(format: "%.0f kg", sender.value))
+        weightLabel.text = String(format: "%.0f kg", sender.value)
+    }
+    
+    @IBAction func calculatedPressed (_ sender: UIButton){
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        let bmi = weight / pow(height, 2)
+        
+        print(bmi)
+    }
+    
+    
 }
 
